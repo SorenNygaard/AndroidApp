2e339a6622c55bf7a5f5234546025b1df15e7879
tknm22 <126458834+tknm22@users.noreply.github.com>
2024-04-19 16:00:13 +0200

    Tilføjet user settings
    User setting og xml filer er blevet tilføjet

---
+++ app/src/main/java/com/example/projekt/UserSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.projekt;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_settings); // Link XML layout file
+    }
+}

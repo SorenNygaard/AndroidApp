68a0c694bca0d8178521f1a81ffab6dd66e7907e
Hanhan <johanne_vigen@hotmail.com>
2024-04-22 15:15:03 +0200

    .

--- app/src/main/java/com/example/projekt/UserSettingsActivity.java
+++ app/src/main/java/com/example/projekt/UserSettingsActivity.java
@@ -1,14 +1,27 @@
 package com.example.projekt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserSettingsActivity extends AppCompatActivity {
-
+   ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings); // Link XML layout file
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
